eeec8892daf92657d95dbb0cc703c807df8e01ad
Tamer Zraiq <G00425053@atu.ie>
2024-03-15 12:54:25 +0000

    Committed pool and standard package

---
+++ src/main/java/ie/atu/jdbc/standard/TestConnection.java
@@ -0,0 +1,34 @@
+package ie.atu.jdbc.standard;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestConnection {
+
+    public static void main(String[] args) {
+
+        Connection connection = null;
+        try
+        {
+            // Load the driver class
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // Create a connection to the database, hardcoding values for now.
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/week8", "root", "password");
+            System.out.println("Connection made to connection pool");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        } finally
+        {
+            // Close the connection when finished
+            if (connection != null) {
+                try
+                {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
